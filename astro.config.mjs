ec52b5be55ec319089068a9d70ca2f8e73642aef
Mateusz Świder <mativve@gmail.com>
2023-08-21 16:31:12 +0200

    System and mobile version
    Set up a relative links
    Add favicon
    
    Setup mobile version on Data, Planets, Planet and WelcomeHero elements
    Recalculate font size scale in base-layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import relativeLinks from 'astro-relative-links';
 
-// https://astro.build/config
 export default defineConfig({
-    root: './'
+  // ...
+  integrations: [relativeLinks()],
 });
